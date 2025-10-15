f6b7bbc2c901e423af1deebe3b231ab4ab69db71
Markos <markoskatehis@gmail.com>
2025-10-15 05:31:29 -0400

    Lab submission

--- src/components/JokeDisplay.jsx
+++ src/components/JokeDisplay.jsx
@@ -1,10 +1,13 @@
-// Step 1: Accept `joke` and `loading` as props
+import React from "react"
 
 const JokeDisplay = ({ joke, loading }) => {
   return (
     <div className="joke-container">
-      {/* Step 2: If `loading` is true, display "Loading..." */}
-      {/* Step 3: Otherwise, display the joke */}
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <p>{joke}</p>
+      )}
     </div>
   )
 }
